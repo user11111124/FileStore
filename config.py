ddb8ce752ab8df89aa5c834a96e668dc7f8be02b
David <daveman17.laptop@gmail.com>
2025-03-01 09:09:38 +0100

    Update config.py

--- config.py
+++ config.py
@@ -40,7 +40,7 @@ START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/ec17880d61180d3
 FORCE_PIC = os.environ.get("FORCE_PIC", "https://telegra.ph/file/e292b12890b8b4b9dcbd1.jpg")
 
 HELP_TXT = "<b>ᴛʜɪs ɪs ᴀɴ 𝕏ғɪʟᴇ ʙᴏᴛ ғᴏʀ @xtveurope\n\n❏ ʙᴏᴛ ᴄᴏᴍᴍᴀɴᴅs\n├/start : sᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ\n├/about : ᴏᴜʀ Iɴғᴏʀᴍᴀᴛɪᴏɴ\n└/help : ʜᴇʟᴘ ʀᴇʟᴀᴛᴇᴅ ʙᴏᴛ\n\n sɪᴍᴘʟʏ ᴄʟɪᴄᴋ ᴏɴ ʟɪɴᴋ ᴀɴᴅ sᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ ᴊᴏɪɴ ʙᴏᴛʜ ᴄʜᴀɴɴᴇʟs ᴀɴᴅ ᴛʀʏ ᴀɢᴀɪɴ ᴛʜᴀᴛs ɪᴛ.....!\n\n ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ <a href=https://t.me/xseriesofficial>𝕏𝕊𝕖𝕣𝕚𝕖𝕤</a></b>"
-ABOUT_TXT = "<b>◈ ᴄʀᴇᴀᴛᴏʀ: <a href=https://t.me/davdxp>S0L0™</a>\n◈ ꜱᴇʀɪᴇꜱ ᴄʜᴀɴɴᴇʟ: <a href=https://t.me/xseriesfy>𝕏𝕊𝕖𝕣𝕚𝕖𝕤</a>\n◈ ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href=https://t.me/xtv_official>xᴏꜰꜰɪᴄɪᴀʟ</a></b>"
+ABOUT_TXT = "<b>◈ Main Channel: <a href=https://t.me/xtveurope>𝕏𝕋𝕍</a>\n◈ Series Channel: <a href=https://t.me/xseriesfy>𝕏𝕊𝕖𝕣𝕚𝕖𝕤</a>\n◈ Movie Channel: <a href=https://t.me/xmoviesfy>𝕏𝕄𝕠𝕧𝕚𝕖𝕤</a>\n◈ Anime Channel: <a href=https://t.me/xanimefy>𝕏𝔸𝕟𝕚𝕞𝕖</a>\n◈ Developer: <a href=https://t.me/davdxp>S0L0™</a></b>"
 START_MSG = os.environ.get("START_MESSAGE", "<b>Hello {first}\n\n ɪ ᴀᴍ 𝕏ғɪʟᴇ sᴛᴏʀᴇ ʙᴏᴛ, ɪ ᴄᴀɴ sᴛᴏʀᴇ ғɪʟᴇs.</b>")
 try:
     ADMINS=[]
